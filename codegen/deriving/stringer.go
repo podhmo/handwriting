de063a2b147e3b5b991c2ed5fc07a397b700661e
podhmo <ababjam61+github@gmail.com>
2018-03-29 03:55:52 +0900

    refine stringer

--- codegen/deriving/stringer.go
+++ codegen/deriving/stringer.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/podhmo/handwriting"
+	"github.com/podhmo/handwriting/codegen/lookup"
 	"github.com/podhmo/handwriting/codegen/typesutil"
 	"github.com/podhmo/handwriting/indent"
 )
@@ -21,41 +22,56 @@ func BindStringer(f *handwriting.File, name string) func(e *handwriting.Emitter)
 
 // Stringer :
 func Stringer(pkg *types.Package, name string, o *indent.Output) error {
-	target := pkg.Scope().Lookup(name)
-	if target == nil {
-		return errors.Errorf("%q is not found from package %q", name, pkg.Path())
+	target, err := lookup.Object(pkg, name)
+	if err != nil {
+		return errors.Wrap(err, "lookup target")
 	}
 
+	if types.Identical(target.Type().Underlying(), types.Universe.Lookup("string").Type()) {
+		return stringerForStringType(target, pkg, name, o)
+	}
+
+	return stringerDefault(target, pkg, name, o)
+}
+
+func stringerForStringType(target types.Object, pkg *types.Package, name string, o *indent.Output) error {
+	// todo : reuse
+	typeMap := typesutil.Scan(pkg)
+	o.Println("// String :")
+	o.WithBlock(fmt.Sprintf("func (x %s) String() string", target.Name()), func() {
+		o.Println("switch x {")
+		for _, ob := range typeMap[target.Type()] {
+			if ob, ok := ob.(*types.Const); ok {
+				o.WithIndent(fmt.Sprintf("case %s:", ob.Name()), func() {
+					o.Println(fmt.Sprintf("return %s", ob.Val()))
+				})
+			}
+		}
+		o.WithIndent("default:", func() {
+			o.Printfln(`return fmt.Sprintf("%s(%%q)", string(x))`, target.Name())
+		})
+		o.Println("}")
+	})
+	return nil
+}
+
+func stringerDefault(target types.Object, pkg *types.Package, name string, o *indent.Output) error {
 	// todo : reuse
 	typeMap := typesutil.Scan(pkg)
 
 	o.Println("// String :")
 	o.WithBlock(fmt.Sprintf("func (x %s) String() string", target.Name()), func() {
 		o.Println("switch x {")
-		switch types.Identical(target.Type().Underlying(), types.Universe.Lookup("string").Type()) {
-		case true:
-			for _, ob := range typeMap[target.Type()] {
-				if ob, ok := ob.(*types.Const); ok {
-					o.WithIndent(fmt.Sprintf("case %s:", ob.Name()), func() {
-						o.Println(fmt.Sprintf("return %s", ob.Val()))
-					})
-				}
+		for _, ob := range typeMap[target.Type()] {
+			if ob, ok := ob.(*types.Const); ok {
+				o.WithIndent(fmt.Sprintf("case %s:", ob.Name()), func() {
+					o.Println(fmt.Sprintf("return %q", ob.Name()))
+				})
 			}
-			o.WithIndent("default:", func() {
-				o.Printfln(`return fmt.Sprintf("%s(%%q)", string(x))`, target.Name())
-			})
-		default:
-			for _, ob := range typeMap[target.Type()] {
-				if ob, ok := ob.(*types.Const); ok {
-					o.WithIndent(fmt.Sprintf("case %s:", ob.Name()), func() {
-						o.Println(fmt.Sprintf("return %q", ob.Name()))
-					})
-				}
-			}
-			o.WithIndent("default:", func() {
-				o.Printfln(`return fmt.Sprintf("%s(%%q)", x)`, target.Name())
-			})
 		}
+		o.WithIndent("default:", func() {
+			o.Printfln(`return fmt.Sprintf("%s(%%q)", x)`, target.Name())
+		})
 		o.Println("}")
 	})
 	return nil
